5e6a72f3806f4dca301d9928c406a227de459deb
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-04-14 17:52:16 +0300

    Initial Commit

---
+++ DataDependencySwiftUI/DataDependencySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataDependencySwiftUIApp.swift
+//  DataDependencySwiftUI
+//
+//  Created by Елена Логинова on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DataDependencySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
