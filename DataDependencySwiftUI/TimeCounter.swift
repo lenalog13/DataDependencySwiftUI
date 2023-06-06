5c48d4b77d0d80bb023a3064bdc6e5f0990ff257
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-06-06 18:22:48 +0300

    create TimeCounter

--- DataDependencySwiftUI/TimeCounter.swift
+++ DataDependencySwiftUI/TimeCounter.swift
@@ -7,3 +7,30 @@
 
 import Foundation
 import Combine
+
+class TimeCounter {
+    var counter = 3
+    var timer: Timer?
+    
+    func startTimer() {
+        timer = Timer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(updateCounter),
+            userInfo: nil,
+            repeats: true)
+    }
+    
+    @objc private func updateCounter() {
+        if counter > 0 {
+            counter -= 1
+        } else {
+            killTimer()
+        }
+    }
+    
+    private func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
